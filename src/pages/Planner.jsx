108574ab356ecf9498c74c6534c9d885e235bb88
Brock Fairbanks <brock@fairbanksbuilders.com>
2026-03-16 03:37:03 +0000

    Manual code change

--- src/pages/Planner.jsx
+++ src/pages/Planner.jsx
@@ -5,20 +5,11 @@ import { format } from "date-fns";
 
 import PortraitOverlay from "../components/planner/PortraitOverlay.jsx";
 import GlobalCanvas from "../components/planner/GlobalCanvas.jsx";
-import TemplateRenderer from "../components/planner/TemplateRenderer.jsx";
 import TabBar from "../components/planner/TabBar.jsx";
 import HeaderBar from "../components/planner/HeaderBar.jsx";
 
-const TEMPLATES = {
-  DAILY: "DAILY",
-  IDEAL_WEEK: "IDEAL_WEEK",
-  QUARTERLY_GOALS: "QUARTERLY_GOALS",
-  RITUALS: "RITUALS",
-  WEEKLY_REVIEW: "WEEKLY_REVIEW",
-};
-
 export default function Planner() {
-  const [activeTemplate, setActiveTemplate] = useState(TEMPLATES.DAILY);
+  const [activeTemplate, setActiveTemplate] = useState("DAILY");
   const [selectedDate, setSelectedDate] = useState(new Date());
   const canvasRef = useRef(null);
   const localInkMemory = useRef({});
@@ -41,7 +32,6 @@ export default function Planner() {
       const blob = await res.blob();
       const file = new File([blob], `${pageKey}.png`, { type: "image/png" });
       const { file_url } = await base44.integrations.Core.UploadFile({ file });
-
       if (existingDrawing?.id) {
         return base44.entities.PageDrawing.update(existingDrawing.id, { image_url: file_url });
       }
@@ -64,24 +54,17 @@ export default function Planner() {
     <div className="fixed inset-0 overflow-hidden bg-[#FAF9F6]">
       <PortraitOverlay />
 
-      {/* Layer 1: Navigation (Top) */}
+      {/* Layer 1: Navigation */}
       <div className="relative z-50">
         <TabBar activeTemplate={activeTemplate} onTemplateChange={setActiveTemplate} />
         <HeaderBar selectedDate={selectedDate} onDateChange={setSelectedDate} isSynced={!saveMutation.isPending} />
       </div>
 
-      {/* Layer 0: The Paper (Bottom) */}
-      <div className="fixed left-20 md:left-24 right-0 top-16 md:top-20 bottom-0 z-0 pointer-events-none overflow-hidden bg-[#FAF9F6]">
-        <TemplateRenderer template={activeTemplate} date={selectedDate} />
-      </div>
-
-      {/* Layer 2: The Ink (Middle - Transparent) */}
-      <div 
-        className="fixed left-20 md:left-24 right-0 top-16 md:top-20 bottom-0 z-10 bg-transparent" 
-        style={{ touchAction: "none" }}
-      >
+      {/* Layer 2: All-In-One Flat Engine */}
+      <div className="fixed left-20 md:left-24 right-0 top-16 md:top-20 bottom-0 z-10">
         <GlobalCanvas
           ref={canvasRef}
+          activeTemplate={activeTemplate}
           onSave={handleSaveInk}
           savedImageData={currentImageData}
           pageKey={pageKey}
