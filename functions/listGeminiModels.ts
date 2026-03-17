9b7c87b07cd72a5eec17c317489d09779d2f4234
Brock Fairbanks <brock@fairbanksbuilders.com>
2026-03-17 18:23:43 +0000

    File changes

--- functions/listGeminiModels.ts
+++ functions/listGeminiModels.ts
@@ -2,5 +2,6 @@ const apiKey = Deno.env.get("GEMINI_API_KEY");
 
 Deno.serve(async (req) => {
     const res = await fetch(`https://generativelanguage.googleapis.com/v1beta/models?key=${apiKey}`);
-    return Response.json(await res.json());
+    const data = await res.json();
+    return Response.json(data.models.map(m => m.name));
 });
