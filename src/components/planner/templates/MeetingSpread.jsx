8072aec0f05508c8f7d1454b128938e38a7ed715
Brock Fairbanks <brock@fairbanksbuilders.com>
2026-03-23 22:54:19 +0000

    File changes

--- src/components/planner/templates/MeetingSpread.jsx
+++ src/components/planner/templates/MeetingSpread.jsx
@@ -421,7 +421,7 @@ export default function MeetingSpread({ date, onClearCanvas }) {
       if (isFinal) setProcessingStatus("Saving...");
       setTranscription(prev => {
         const newText = prev ? prev + "\n\n" + text : text;
-        saveNote(newText, null, uploadedFileUrl);
+        saveNote(newText, null, uploadedFileUrl, mimeType.includes('video'));
         return newText;
       });
     } catch (err) {
