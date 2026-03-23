8692588fdbc96614defddface762af3dcec01b0b
Brock Fairbanks <brock@fairbanksbuilders.com>
2026-03-23 22:37:27 +0000

    File changes

--- src/components/planner/templates/WeeklyReview.jsx
+++ src/components/planner/templates/WeeklyReview.jsx
@@ -135,8 +135,8 @@ export default function WeeklyReview({ date, onClearCanvas }) {
                 How will you recharge next week?
               </span>
               <div className="flex-1 w-full mt-2 relative">
-                 <div className="absolute inset-0 border-x border-[#cbd5e1] pointer-events-none" />
-                 <div className="w-full h-full" style={{ backgroundImage: `repeating-linear-gradient(to bottom, transparent, transparent 39px, #cbd5e1 40px)`, backgroundSize: "100% 40px" }} />
+                 <div className="absolute inset-0 border-l border-r border-[#cbd5e1] pointer-events-none" />
+                 <div className="w-full h-full border-t border-[#cbd5e1]" style={{ backgroundImage: `repeating-linear-gradient(to bottom, transparent, transparent 39px, #cbd5e1 40px)`, backgroundSize: "100% 40px" }} />
               </div>
            </div>
         </div>
