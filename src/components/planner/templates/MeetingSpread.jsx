d248d4e962518ae568df290781dacd129ae646e8
Brock Fairbanks <brock@fairbanksbuilders.com>
2026-03-23 20:43:20 +0000

    File changes

--- src/components/planner/templates/MeetingSpread.jsx
+++ src/components/planner/templates/MeetingSpread.jsx
@@ -144,10 +144,10 @@ export default function MeetingSpread({ date, onClearCanvas }) {
 
   useEffect(() => {
     fetchNotes();
-  }, []);
+  }, [user?.email]);
 
   const fetchNotes = async () => {
-    if (!user) return;
+    if (!user?.email) return;
     try {
       const notes = await base44.entities.MeetingNote.filter({ created_by: user.email }, "-created_date", 50);
       setSavedNotes(notes);
